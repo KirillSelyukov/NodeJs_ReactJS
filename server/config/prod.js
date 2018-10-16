42f1d07423474726c8a420c3729ccc68c291fdd1
Kirill Selyukov <kselukov@gmail.com>
2018-10-16 13:28:43 +0300

    Add dev and prod configs

---
+++ server/config/prod.js
@@ -0,0 +1,13 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  mongoUserName: process.env.MONGO_USER_NAME,
+  mongoUserPwd: process.env.MONGO_USER_PWD, //'11?+B5UdE',
+  cookieKey: process.env.COOKIE_KEY
+  // mongoURI: 'mongodb://ds031882.mlab.com:31882/kselukov-emaily-prod',
+  // mongoUserName: 'prod-db-user',
+  // mongoUserPwd: '11?+B5UdE',
+  // googleClientID:'314530200865-2o9elom90pr8kfe5n2ojagups1q8kcb9.apps.googleusercontent.com',
+  // googleClientSecret:'hy7unIuDhchGFbU78QmpeF4R'
+};
