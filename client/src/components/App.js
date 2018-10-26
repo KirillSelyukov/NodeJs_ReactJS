00c73d48a24d8d5b784f13b633711f6dfcfad42b
Kirill Selyukov <kselukov@gmail.com>
2018-10-26 14:08:08 +0300

    Add Header, and Dashboard component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-const Header = () => <h1>Header</h1>;
-const Dashboard = () => <h1>Dashboard</h1>;
+
+import Header from './Header';
+import Dashboard from './Dashboard';
+
+// const HeDashboardader = () => <h1>Header</h1>;
+//const Dashboard = () => <h1>Dashboard</h1>;
 const SurveyNew = () => <h1>SurveyNew</h1>;
 const Landing = () => <h1>Landing</h1>;
 
